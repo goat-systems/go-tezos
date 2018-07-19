d43e3680b623571fa2ae82d494e4c73f3612284d
DefinitelyNotAGoat <tezosredemption@protonmail.com>
2018-07-19 19:00:12 -0400

    adding unit tests

---
+++ Tests/goTezosTests.go
@@ -0,0 +1,19 @@
+package main
+/*
+Author: DefinitelyNotAGoat
+Description: This is a program to test all functions in the goTezos library.
+*/
+
+import (
+  "fmt"
+)
+
+type Config struct{
+  delegateAddr string
+  
+}
+
+
+func main() {
+
+}
