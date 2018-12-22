15f8d8d05094698bc0cf33bdd8a1406aca3014cb
Matthew Boehm <github@matthewboehm.com>
2018-12-21 22:34:14 -0600

    Added support for web-RPC endpoints (ie: https://rpc.tzbeta.net)

--- goTezosNode.go
+++ goTezosNode.go
@@ -12,22 +12,39 @@ import (
 )
 
 type TezosRPCClient struct {
-	Host        string
-	Port        string
-	logfunction func(level, msg string)
-	logger *log.Logger
+	Host          string
+	Port          string
+	logfunction   func(level, msg string)
+	logger        *log.Logger
+	isWebClient   bool
 }
 
-
-
-func NewTezosRPCClient(hostname string, port string) *TezosRPCClient{
+// Create a new RPC client using the specified hostname and port.
+// Also acceptable is the hostname of a web-endpoint that supports https
+func NewTezosRPCClient(hostname string, port string) *TezosRPCClient {
+	
 	t := TezosRPCClient{}
+	
+	// Strip off posible trailing '/'
+	hLen := len(hostname)
+	if hostname[hLen - 1] == '/' {
+		hostname = hostname[:hLen - 1]
+	}
+	
+	// Strip off URI scheme
+	if hostname[:8] == "https://" {
+		hostname = hostname[8:]
+		t.isWebClient = true
+	} else if hostname[:7] == "http://" {
+		hostname = hostname[7:]
+	}
+	
 	t.Host = hostname
 	t.Port = port
 	t.logfunction = func(level, msg string) {
 		fmt.Println(level + ": " + msg)
 	}
-	t.SetLogger(log.New(os.Stdout,hostname,0))
+	t.SetLogger(log.New(os.Stdout, hostname, 0))
 	return &t
 }
 
@@ -37,9 +54,21 @@ func (this *TezosRPCClient) SetLogger(log *log.Logger) {
 }
 
 
-func (this *TezosRPCClient) GetResponse(method string, path string, args string) (ResponseRaw, error) {
+func (this *TezosRPCClient) IsWebClient(b bool) {
+	this.isWebClient = b
+}
 
-	url := "http://" + this.Host + this.Port + "" + path
+
+func (this *TezosRPCClient) GetResponse(method string, path string, args string) (ResponseRaw, error) {
+	
+	var url string
+	
+	if this.isWebClient {
+		url = fmt.Sprintf("https://%s:%s%s", this.Host, this.Port, path)
+	} else {
+		url = fmt.Sprintf("http://%s:%s%s", this.Host, this.Port, path)
+	}
+	
 	var jsonStr = []byte(args)
 	req, err := http.NewRequest(method, url, bytes.NewBuffer(jsonStr))
 	if err != nil {
