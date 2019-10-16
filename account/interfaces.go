86e4988b459646a4724aff58e9970ba0626d62ab
utdrmac <github@matthewboehm.com>
2019-10-16 15:23:09 +0000

    added getting the delegate for address at block

--- account/interfaces.go
+++ account/interfaces.go
@@ -4,6 +4,7 @@ type TezosAccountService interface {
 	GetBalanceAtSnapshot(tezosAddr string, cycle int) (float64, error)
 	GetBalance(tezosAddr string) (float64, error)
 	GetBalanceAtBlock(tezosAddr string, id interface{}) (float64, error)
+	GetDelegateAtBlock(tezosAddr string, id interface{}) (string, error)
 	CreateWallet(mnenomic string, password string) (Wallet, error)
 	ImportWallet(address, public, secret string) (Wallet, error)
 	ImportEncryptedWallet(pw, encKey string) (Wallet, error)
