e615eb271ce0e80c94dfbcb74035878681b57ca1
Brice Aldrich <40796124+DefinitelyNotAGoat@users.noreply.github.com>
2020-02-25 22:19:29 -0500

    adding iface (#125)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
+## [v2.4.1-alpha] 
+ 
+Adding IFace for ease of testing with the library.
+
 ## [v2.3.1-alpha] 
  
 All balances now have big.Int.
---
+++ iface.go
@@ -0,0 +1,41 @@
+package gotezos
+
+import "math/big"
+
+// IFace is an interface mocking a GoTezos object.
+type IFace interface {
+	ActiveChains() (*ActiveChains, error)
+	BakingRights(input *BakingRightsInput) (*BakingRights, error)
+	Balance(blockhash, address string) (*big.Int, error)
+	Block(id interface{}) (*Block, error)
+	Blocks(input *BlocksInput) (*[][]string, error)
+	Bootstrap() (*Bootstrap, error)
+	ChainID() (*string, error)
+	Checkpoint() (*Checkpoint, error)
+	Commit() (*string, error)
+	Connections() (*Connections, error)
+	Constants(blockhash string) (*Constants, error)
+	ContractStorage(blockhash string, KT1 string) (*[]byte, error)
+	Counter(blockhash, pkh string) (*int, error)
+	Cycle(cycle int) (*Cycle, error)
+	Delegate(blockhash, delegate string) (*Delegate, error)
+	Delegates(input *DelegatesInput) (*[]string, error)
+	DelegatedContracts(blockhash, delegate string) (*[]string, error)
+	DelegatedContractsAtCycle(cycle int, delegate string) (*[]string, error)
+	DeleteInvalidBlock(blockHash string) error
+	EndorsingRights(input *EndorsingRightsInput) (*EndorsingRights, error)
+	ForgeOperation(branch string, contents ...Contents) (*string, error)
+	FrozenBalance(cycle int, delegate string) (*FrozenBalance, error)
+	Head() (*Block, error)
+	InjectionBlock(input *InjectionBlockInput) (*[]byte, error)
+	InjectionOperation(input *InjectionOperationInput) (*[]byte, error)
+	InvalidBlock(blockHash string) (*InvalidBlock, error)
+	InvalidBlocks() (*[]InvalidBlock, error)
+	OperationHashes(blockhash string) (*[]string, error)
+	PreapplyOperations(blockhash string, contents []Contents, signature string) (*[]byte, error)
+	StakingBalance(blockhash, delegate string) (*big.Int, error)
+	StakingBalanceAtCycle(cycle int, delegate string) (*big.Int, error)
+	UnforgeOperation(operation string, signed bool) (*string, *[]Contents, error)
+	UserActivatedProtocolOverrides() (*UserActivatedProtocolOverrides, error)
+	Version() (*Version, error)
+}
